d934f851e6a234a5bd7a10fd386a805b74897c34
Patt's Mac Mini <pattobrien@themachinemedia.com>
2021-10-16 16:16:36 -0400

    Added install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+# Install XCode CLI tools
+xcode-select --Install
+
+# ...or use HTTPS and switch remotes later.
+git clone https://github.com/pattobrien/environment.git ~/.dotfiles
+
+# Creating symlinks for zshrc and gitconfig files
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Then pass in the Brewfile location...
+brew bundle --file ~/.dotfiles/Brewfile
