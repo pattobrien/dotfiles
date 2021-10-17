1e19f5786656e23468085ef9dc609a771bb4ca71
Patt's Mac Mini <pattobrien@themachinemedia.com>
2021-10-17 00:58:15 -0400

    add docker install script

---
+++ Scripts/docker-install.sh
@@ -0,0 +1,8 @@
+# install docker
+# by visiting: https://docs.docker.com/desktop/mac/install/
+# and downloading Docker for Mac DMG file
+
+# open Docker for Mac application manually
+
+# install Docker-Compose
+brew install docker-compose
