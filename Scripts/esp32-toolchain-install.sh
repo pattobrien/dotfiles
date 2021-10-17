da9469c3b9915c3716fc56cf3f30e05ddcea1209
Patt's Mac Mini <pattobrien@themachinemedia.com>
2021-10-17 01:28:12 -0400

    preliminary esp32 toolchain install script

---
+++ Scripts/esp32-toolchain-install.sh
@@ -0,0 +1,11 @@
+# for reference:
+# https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/macos-setup.html
+
+# install pip
+sudo easy_install pip
+
+# install CMake & Ninja build:
+brew install cmake ninja dfu-util
+
+# It is strongly recommended to also install ccache for faster builds. 
+brew install ccache
