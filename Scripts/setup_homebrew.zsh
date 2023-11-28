1149d725066888f04d69e32a295eb58afcd0032a
pattobrien <12474318+pattobrien@users.noreply.github.com>
2023-11-28 03:29:27 -0500

    Updated all local brew versions

---
+++ Scripts/setup_homebrew.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Setting up Homebrew >>>\n"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# disable gatekeeping just for the initial install
+export HOMEBREW_CASK_OPTS="--no-quarantine"
+
+brew bundle --verbose --file brew/Brewfile
