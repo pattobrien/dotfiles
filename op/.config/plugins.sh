8ac3d81594d67187f4cf04382d6e26f4b14e7bfb
pattobrien <12474318+pattobrien@users.noreply.github.com>
2023-12-02 23:39:07 -0500

    fix: Brew prefix rather than static brew bin paths.

--- op/.config/plugins.sh
+++ op/.config/plugins.sh
@@ -1,7 +1,8 @@
 export OP_PLUGIN_ALIASES_SOURCED=1
 
-alias doctl="op plugin run -- doctl"
-alias gh="op plugin run -- gh"
+# doctl causes 1password prompt when re-sourcing zshrc file, for some reason... so we should not use this
+# alias doctl="op plugin run -- doctl"
+# alias gh="op plugin run -- gh"
 alias openai="op plugin run -- openai"
 alias terraform="op plugin run -- terraform"
 alias wrangler="op plugin run -- wrangler"
