9aa8b4b753c234092c205746c4438f5091f9cf58
pattobrien <12474318+pattobrien@users.noreply.github.com>
2025-12-04 11:12:20 -0500

    feat: pnpm autocomplete and aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,28 @@
 alias k="kubectl"
 
 alias gc="git commit -m"
-alias gadd="git add ."
+alias ga="git add ."
+alias gs="git status"
+
+alias p="pnpm"
+alias pi="pnpm install"
+alias pd="pnpm dev"
+alias pf="pnpm format"
+alias pl="pnpm lint"
+alias pt="pnpm test"
+alias px="pnpx"
+
+alias jq="jq -C"  # always colorize
+
+alias l="ls -la"
+alias ll="ls -lah"
+
+alias cat="bat"
+alias grep="rg"
+alias find="fd"
+
+alias preview="fzf --preview 'bat --color=always {}'" # preview files in fzf
+alias branches="git branch | fzf | xargs git switch" # switch to a branch in fzf
 
 # alias npm="pnpm"
 # alias npx="pnpx"
