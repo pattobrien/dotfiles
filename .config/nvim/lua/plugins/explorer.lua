d7b608568e8ad6c3ac7e841d35d1ab203eb3a11d
pattobrien <12474318+pattobrien@users.noreply.github.com>
2026-03-31 21:47:18 -0400

    feat(nvim): show hidden dotfiles in explorer, file search, and grep

--- .config/nvim/lua/plugins/explorer.lua
+++ .config/nvim/lua/plugins/explorer.lua
@@ -3,8 +3,29 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        icons = {
+          git = {
+            untracked = "U ",
+            added = "A ",
+            modified = "M ",
+            deleted = "D ",
+            renamed = "R ",
+            staged = "● ",
+          },
+        },
+        formatters = {
+          file = {
+            git_status_hl = true,
+          },
+        },
         sources = {
           explorer = {
+            hidden = true,
+          },
+          files = {
+            hidden = true,
+          },
+          grep = {
             hidden = true,
           },
         },
